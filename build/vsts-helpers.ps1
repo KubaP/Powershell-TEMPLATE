9c7af5650af514203855b976926697b26e911623
KubaP <git@kubap.io>
2021-02-26 20:37:51 +0000

    Update formatting of vsts scripts
    - Rename `WriteHeader` to `Write-Header` to follow powershell best practices.
    - Format comments and improve help pages.
    - Simplify the `Write-Header` function.

--- build/vsts-helpers.ps1
+++ build/vsts-helpers.ps1
@@ -1,4 +1,4 @@
-function WriteHeader {
+function Write-Header {
 	param
 	(
 		[Parameter(Position = 0, Mandatory = $true)]
@@ -7,26 +7,20 @@ function WriteHeader {
 		
 		[Parameter(Position = 1)]
 		[string]
-		[ValidateSet("Red", "Green", "Blue", "Cyan", "Magenta", "Yellow", "white", "Black")]
+		[ValidateSet("Red", "Green", "Blue", "Cyan", "Magenta", "Yellow", "White", "Black")]
 		$Colour
 	)
 	
 	# Initialize the variable to output.
-	$HelpMessage = ''
+	$output = ''
 	
 	# Add some extra spacing to either side of the message.
 	$length = $Message.Length + 2
 	
-	function WriteBorder($remark) {
-		$HelpMessage += "+$("-" * $length)+$([Environment]::NewLine)" 
-	}
-
-	. WriteBorder 'Top'
-	
-	$HelpMessage += "| $Message |$([Environment]::NewLine)" 
-	
-	. WriteBorder 'bottom'
+	$output += "+$("-" * $length)+$([Environment]::NewLine)" 
+	$output += "| $Message |$([Environment]::NewLine)" 
+	$output += "+$("-" * $length)+$([Environment]::NewLine)" 
 	
 	# Write the text.
-	Write-Host $HelpMessage -ForegroundColor $Colour
+	Write-Host $output -ForegroundColor $Colour
 }
